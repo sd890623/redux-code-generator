b41c6e7503865c4033c7096f2198f1fd338c3417
daniel sun <daniel.sun@hunterindustries.com>
2017-07-11 18:09:22 +1000

    update template

---
+++ generators/app/templates/ObjectContainer.js
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux';
+import <%= name %>View from './<%= name %>View';
+import {
+
+} from './<%= name %>State';
+
+export default connect(
+  state => ({
+}),
+dispatch => ({
+})
+)(<%= name %>View);
