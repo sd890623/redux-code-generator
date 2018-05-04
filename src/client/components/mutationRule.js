dd49ecd2b6833d9d5465baedef2ef17828bcdb7b
Daniel Sun <daniel.sun@hunterindustries.com>
2018-05-04 15:59:35 +1000

    Ready to create mutation rule

--- src/client/components/mutationRule.js
+++ src/client/components/mutationRule.js
@@ -1,3 +1,18 @@
-export function generateMutationGraph(variableArray) {
-	
+import React from 'react';
+
+export default function generateMutationGraph(variableArray) {
+  return (
+    <pre className="result-panel">
+      {`
+	      mutation createWateringProgram(
+	        $wateringProgramName: String!,
+	        $wateringProgramName: String!) {
+	        createWateringProgram (
+	          wateringProgramName: $wateringProgramName,
+	          wateringProgramName: $wateringProgramName
+	        )
+	      }
+	    `}
+    </pre>
+  );
 }
--- src/client/components/queryGenerator.js
+++ src/client/components/queryGenerator.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Button, Form, FormGroup, ControlLabel, FormControl, Checkbox } from 'react-bootstrap';
 import bindAll from 'lodash/bindAll';
 import findIndex from 'lodash/findIndex';
+import generateMutationGraph from './mutationRule';
 
 class QueryGenerator extends Component {
   constructor() {
@@ -10,10 +11,13 @@ class QueryGenerator extends Component {
       variableName: '',
       variableType: 0,
       variableCompulsory: false,
-	    variables: []
+	    executeOption: 0,
+	    variables: [],
+      mutationKeyWord: ''
     });
     this.typeArray = [{ value: 0, label: 'String' }, { value: 1, label: 'Int' }, { value: 2, label: 'Boolean' }];
-	  bindAll(this, ['submitVariable', 'findTypeLabelFromValue']);
+    this.optionArray = [{ value: 0, label: 'ApolloMutation' }, { value: 1, label: 'ApolloQuery' }];
+	  bindAll(this, ['submitVariable', 'findTypeLabelFromValue', 'saveToStorage', 'loadFromStorage', 'execute']);
   }
 
   findTypeLabelFromValue(typeValue) {
@@ -22,6 +26,20 @@ class QueryGenerator extends Component {
       return this.typeArray[index].label;
     }
     return 'unknown';
+  }
+
+  saveToStorage() {
+    window.localStorage.variables = JSON.stringify(this.state.variables);
+  }
+
+  loadFromStorage() {
+    if (window.localStorage.variables) {
+      this.setState({ variables: JSON.parse(window.localStorage.variables) });
+      console.log(JSON.parse(window.localStorage.variables));
+    }
+  }
+  execute() {
+
   }
 
   submitVariable() {
@@ -34,7 +52,8 @@ class QueryGenerator extends Component {
   render() {
     return (
       <div className="container">
-        <Form inline>
+        <h1>Why not make it automatic</h1>
+        <Form inline id="first-row">
           <FormGroup className="m-r-5">
             <ControlLabel>Name</ControlLabel>
             <FormControl
@@ -48,10 +67,10 @@ class QueryGenerator extends Component {
             componentClass="select"
             className="m-r-5"
             value={this.state.variableType}
-            onChange={e => this.setState({ variableType: e.target.value })}
+            onChange={e => this.setState({ variableType: parseInt(e.target.value, 10) })}
           >
             {this.typeArray.map(type => (
-              <option value={type.value}>{type.label}</option>
+              <option key={type.value} value={type.value}>{type.label}</option>
 	          ))}
           </FormControl>
           <FormGroup>
@@ -65,7 +84,7 @@ class QueryGenerator extends Component {
             <Button bsStyle="primary" onClick={this.submitVariable}>Submit</Button>
           </FormGroup>
         </Form>
-        <div className="row">
+        <div className="row" id="second-row">
           <div className="col-sm-6">
             <h2 className="variables-title">Saved variables</h2>
             <table className="variables-container">{this.state.variables.map(variableSet => (
@@ -76,8 +95,45 @@ class QueryGenerator extends Component {
             </table>
           </div>
           <div className="col-sm-6">
-            {123}
+            <h2 className="result-title">Results</h2>
+            {generateMutationGraph(this.state.variables)}
           </div>
+        </div>
+        <div className="row btn-set" id="third-row">
+          <Form inline>
+            <FormGroup className="m-r-5">
+              <FormControl
+                componentClass="select"
+                className="m-r-10"
+                value={this.state.executeOption}
+                onChange={e => this.setState({ executeOption: parseInt(e.target.value, 10) })}
+              >
+                {this.optionArray.map(option => (
+                  <option key={option.value} value={option.value}>{option.label}</option>
+                ))}
+              </FormControl>
+              {this.state.executeOption === 0 ?
+                <FormGroup className="m-r-5">
+                  <ControlLabel>Mutation method name</ControlLabel>
+                  <FormControl
+                    type="text"
+                    placeholder="Mutation method name"
+                    value={this.state.mutationKeyWord}
+                    onChange={e => this.setState({ mutationKeyWord: e.target.value })}
+                  />
+                </FormGroup> : ''
+	            }
+            </FormGroup>
+            <Button onClick={this.saveToStorage} bsStyle="primary" className="m-r-5">
+              Store input to local storage
+            </Button>
+            <Button onClick={this.loadFromStorage} bsStyle="primary" className="m-r-5">
+              Load saved inputs to app
+            </Button>
+            <Button onClick={this.execute} bsStyle="primary">
+              Execute
+            </Button>
+          </Form>
         </div>
       </div>
     );
